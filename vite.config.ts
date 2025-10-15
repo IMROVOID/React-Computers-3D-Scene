498755bfd228048b5828e8611e7888fb730b9bcf
ROVOID <141726490+IMROVOID@users.noreply.github.com>
2025-10-15 20:02:59 +0330

    some small Modifications

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
   
   // https://vitejs.dev/config/
   export default defineConfig({
+      base: "/React-Computers-3D-Scene/",
       plugins: [react()],
   });
-  
